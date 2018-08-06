15bb7918adbe5ed16986bd44f4067fc983ba54da
Abraham Ibarra <aibarra988@gmail.com>
2018-08-05 22:01:10 -0500

    added gif pausing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -58,3 +58,13 @@ $(document).on("click", ".search-button", function() {
     });
 });
 
+$(document).on("click", ".gif-image", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));   
+        $(this).attr("data-state", "animate");  
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));   
+        $(this).attr("data-state", "still");  
+    }
+});
